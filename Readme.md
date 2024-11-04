9c546b725c5c51c2457fa2a99f99c0d572809693
Yanina.Matuzava <yaninochka2008@gmail.com>
2024-11-04 19:01:18 +0200

    Hw-12

--- Readme.md
+++ Readme.md
@@ -19,17 +19,7 @@
 | 2 | POST login: returns 200 status code with correct data          |        |
 | 3 | POST login: returns 200 status code with valid JWT token       |        |
 | 4 | PUT login: returns 405 status code with incorrect HTTTP method |        |
-=======
-| № | Scenario name                                             | Status |
-|---|-----------------------------------------------------------|--------|
-| 1 | GET order: returns order with correct order ID            |        |
-| 2 | POST order: create order with correct data                |        |
-| 3 | GET order: returns 400 status code with icorrect order ID |        |
-| 4 | POST order: returns 400 status code with incorrect data   |        |
-| 5 | PUT order: update order successfully with correct data    |        |
-| 6 | PUT order: returns 400 status code with incorrect data    |        |
-| 7 | DELETE order: delete order successfully with correct data |        |
-| 8 | DELETE order: returns 400 status code with incorrect data |        |
+
 
 # Checklist for loan
 
---
+++ tests/api/api-client.ts
@@ -0,0 +1,93 @@
+import { APIRequestContext } from 'playwright'
+import { LoginDto } from '../dto/login-dto'
+import { StatusCodes } from 'http-status-codes'
+import { expect } from '@playwright/test'
+import { OrderDto } from '../dto/order-dto'
+
+const serviceURL = 'https://backend.tallinn-learning.ee/'
+const loginPath = 'login/student'
+const orderPath = 'orders'
+
+export class ApiClient {
+  static instance: ApiClient
+  private request: APIRequestContext
+  private jwt: string = ''
+
+  private constructor(request: APIRequestContext) {
+    this.request = request
+  }
+
+  public static async getInstance(request: APIRequestContext): Promise<ApiClient> {
+    if (!ApiClient.instance) {
+      ApiClient.instance = new ApiClient(request)
+      await this.instance.requestJwt()
+    }
+    return ApiClient.instance
+  }
+
+  private async requestJwt(): Promise<void> {
+    console.log('Requesting JWT...')
+    const authResponse = await this.request.post(`${serviceURL}${loginPath}`, {
+      data: LoginDto.createLoginWithCorrectCredentials(),
+    })
+    // Check response status for negative cases
+    if (authResponse.status() !== StatusCodes.OK) {
+      console.log('Authorization failed')
+      throw new Error(`Request failed with status ${authResponse.status()}`)
+    }
+
+    // Save the JWT token as a client property
+    this.jwt = await authResponse.text()
+    console.log('jwt received:')
+    console.log(this.jwt)
+  }
+
+  async createOrderAndReturnOrderId(): Promise<number> {
+    console.log('Creating order...')
+    const response = await this.request.post(`${serviceURL}${orderPath}`, {
+      data: OrderDto.createOrderWithUndefinedOrderId(),
+      headers: {
+        Authorization: `Bearer ${this.jwt}`,
+      },
+    })
+    console.log('Order response: ', response)
+
+    expect(response.status()).toBe(StatusCodes.OK)
+    const responseBody = await response.json()
+    console.log('Order created: ')
+    console.log(responseBody)
+
+    return responseBody.id
+  }
+
+  async getOrderId(orderId: number): Promise<void> {
+    console.log(`Fetching order with ID ${orderId}`)
+    const response = await this.request.get(`${serviceURL}${orderPath}/${orderId}`, {
+      headers: {
+        Authorization: `Bearer ${this.jwt}`,
+      },
+    })
+    console.log('Order ID: ', response)
+
+    expect(response.status()).toBe(StatusCodes.OK)
+    const order = await response.json()
+    const { id } = order
+    expect.soft(id).toBeDefined()
+    expect.soft(id).toBe(orderId)
+  }
+
+  async deleteOrderId(orderId: number): Promise<void> {
+    console.log('Deleting order...')
+    const response = await this.request.delete(`${serviceURL}${orderPath}/${orderId}`, {
+      headers: {
+        Authorization: `Bearer ${this.jwt}`,
+      },
+    })
+    console.log('Delete response: ', response)
+
+    expect(response.status()).toBe(StatusCodes.OK)
+    const body = await response.json()
+    console.log('This order was deleted: ')
+    console.log(body)
+  }
+}
